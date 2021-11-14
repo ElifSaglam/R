c50fc456fba87287ec6b236992fc0135893b25b5
Elif SAĞLAM <61660262+ElifSaglam@users.noreply.github.com>
2021-11-14 14:51:22 +0300

    Add files via upload

---
+++ R-PostgreSqlDbConnection.R
@@ -0,0 +1,37 @@
+### libraries ####
+require(RPostgreSQL)
+require(DBI)
+### End Region ####
+
+
+### db kill connections####
+killDbConnections <- function () {
+  
+  all_cons <- dbListConnections(PostgreSQL())
+  for(con in all_cons)
+    dbDisconnect(con)
+  
+}
+
+killDbConnections()
+### End Region ####
+
+### PostgeSql db Connections ####
+
+drv <- dbDriver("PostgreSQL")
+connection <- dbConnect(
+  drv,
+  dbname = "dss_db",
+  host = "host",
+  port = "port",
+  user = "uzername",
+  password = "password"
+)
+
+
+query <- dbGetQuery(connection, "SELECT * FROM table_name")
+dbDisconnect(connection) 
+
+### End Region ####
+
+
