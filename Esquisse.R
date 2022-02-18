1877f09249e6a05a0362f1ee83c63c1f7f399409
Elif SAĞLAM <61660262+ElifSaglam@users.noreply.github.com>
2022-02-18 17:12:00 +0300

    Add files via upload

---
+++ Esquisse.R
@@ -0,0 +1,17 @@
+
+# Library
+library(esquisse)
+
+# Example
+esquisser()
+esquisser(iris)
+
+# Esquisser options
+esquisser(viewer = "browser")
+esquisser(viewer = "pane")
+options("esquisse.viewer" = "browser")
+
+
+# resources
+# https://github.com/dreamRs/esquisse
+# https://cran.r-project.org/web/packages/esquisse/vignettes/get-started.html
